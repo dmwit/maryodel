85bb430c3f3e029b36e2b3a89f993d5c1a30a85f
Daniel Wagner <me@dmwit.com>
2026-05-05 20:59:30 -0400

    some more pretty-printing stuff

--- haskell/Dr/Mario/PP.hs
+++ haskell/Dr/Mario/PP.hs
@@ -3,11 +3,13 @@ module Dr.Mario.PP where
 import Data.Aeson
 import Data.Foldable
 import Data.List
+import Data.Map (Map)
 import Data.Sequence (Seq)
 import Numeric
 import System.IO
 
 import qualified Data.ByteString.Lazy.Char8 as LBS8
+import qualified Data.Map as M
 import qualified Data.Text as T
 
 -- | Efficiency? What's that?
@@ -23,11 +25,18 @@ instance PP Bool where
 	pp True = "✓"
 	pp False = "✗"
 
-instance PP1 [] where liftPP1 ppElem as = "[" ++ intercalate ", " (map ppElem as) ++ "]"
-instance PP1 Seq where liftPP1 ppElem = liftPP1 ppElem . toList
+instance PP a => PP [a] where pp = pp1
+instance PP a => PP (Seq a) where pp = pp1
 instance (PP a, PP b) => PP (a, b) where pp = pp2
+instance (PP k, PP v) => PP (Map k v) where pp = pp2
+
+instance PP1 [] where liftPP1 ppElem = ppSequence "[]" . map ppElem
+instance PP1 Seq where liftPP1 ppElem = ppSequence "<>" . fmap ppElem
 instance PP a => PP1 ((,) a) where liftPP1 = liftPP2 pp
-instance PP2 (,) where liftPP2 ppA ppB (a, b) = "(" ++ ppA a ++ ", " ++ ppB b ++ ")"
+instance PP k => PP1 (Map k) where liftPP1 = liftPP2 pp
+
+instance PP2 (,) where liftPP2 ppA ppB (a, b) = ppSequence "()" [ppA a, ppB b]
+instance PP2 Map where liftPP2 ppk ppv = ppSequence "{}" . M.mapWithKey (\k v -> ppk k ++ "↦" ++ ppv v)
 
 ppIO :: PP a => a -> IO ()
 ppIO a = putStrLn (pp a) >> hFlush stdout
@@ -59,6 +68,15 @@ elideTo :: Int -> String -> String
 elideTo n s = case drop n s of
 	[] -> s
 	_ -> take (n-3) s ++ "..."
+
+-- | The first argument is split in half, and the second argument is put
+-- inside.
+surround :: String -> String -> String
+surround brackets s = b ++ s ++ e where (b, e) = splitAt (length brackets `div` 2) brackets
+
+-- | Pass delimiters as the first argument.
+ppSequence :: Foldable f => String -> f String -> String
+ppSequence brackets = surround brackets . intercalate ", " . toList
 
 ppPrecision :: Int -> Float -> String
 ppPrecision p n = if isNaN n then "nan" else showFFloat Nothing (fromInteger (round (pow*n))/pow) ""
